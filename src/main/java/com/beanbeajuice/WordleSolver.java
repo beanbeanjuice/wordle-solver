9ab1e1091e8d38fdf1f6665b1a2d73bc15b66d75
William Orlopp <beanbeanjuice@outlook.com>
2022-01-28 12:28:18 -0800

    Created Solver
    Need to remove duplicate letters when solving.

--- src/main/java/com/beanbeajuice/WordleSolver.java
+++ src/main/java/com/beanbeajuice/WordleSolver.java
@@ -1,13 +1,16 @@
 package com.beanbeajuice;
 
 import com.beanbeajuice.helper.FileHelper;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class WordleSolver {
 
     public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
         FileHelper wordFileReader = new FileHelper();
         ArrayList<String> words = wordFileReader.readFile(wordFileReader.getFile("words.txt"));
 
@@ -19,8 +22,50 @@ public class WordleSolver {
             }
         });
 
-        fiveLetterWords.forEach(System.out::println);
+        ArrayList<String> availableWords = new ArrayList<>(fiveLetterWords);
 
+        boolean wordFound = false;
+        String currentInput = "*****";
+        while (!wordFound) {
+
+            ArrayList<Character> characters = new ArrayList<>();
+
+            for (int i = 0; i < currentInput.length(); i++) {
+                characters.add(currentInput.charAt(i));
+            }
+
+            ArrayList<String> tempArray = new ArrayList<>();
+            availableWords.forEach((word) -> {
+
+                if (checkWord(word, characters)) {
+                    tempArray.add(word);
+                }
+
+            });
+
+            availableWords = new ArrayList<>(tempArray);
+
+            System.out.println("Possible Words");
+            availableWords.forEach(System.out::println);
+
+            do {
+                System.out.print("Enter Word (5 Letters Only): ");
+                currentInput = scanner.next();
+            } while (currentInput.length() != 5);
+        }
+
+    }
+
+    public static boolean checkWord(@NotNull String word, @NotNull ArrayList<Character> checker) {
+        for (int i = 0; i < 5; i++) {
+            if (checker.get(i) != '*') {
+                if (word.charAt(i) != checker.get(i)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 
 }
